64fdf2d68f293edc7d92f853dfe11805630a128e
Jacob H. Haven <QuicksilverJohny@gmail.com>
2014-02-19 10:39:40 -0800

    Support for comments and deletion. Added some pretty sample CSS.

--- application.py
+++ application.py
@@ -4,19 +4,18 @@ from flask import request
 from flask import abort
 from flask import redirect
 from markdown import Markdown
+from flask import Markup
 from flask.ext.sqlalchemy import SQLAlchemy
 import bleach
 import os
-import pyscopg2
+import psycopg2
+
 
 from models import *
 
 application = Flask(__name__)
-
-#dev_url = 'sqlite:////' + os.path.dirname(os.path.realpath(__file__)) + 'dev.db'
-#Set up database, connection to server specified by environment variable DATABASE_URL
 application.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(application)
+db.app = application
 db.init_app(application)
 db.create_all()
 
@@ -25,31 +24,70 @@ safe_markdown = Markdown(safe_mode='escape')
 ### Routes
 @application.route('/')
 def index_page():
-  return render_template('index.html')
+  try:
+    blog_posts = BlogPost.query.order_by("timestamp")
+  except:
+    abort(500)
+  return render_template('index.html', blog_posts=blog_posts)
 
 @application.route('/compose', methods=['GET', 'POST'])
 def compose_page():
   if request.method == 'GET':
     return render_template('compose.html')
   else:
-    #try:
-    parsed_post_name = bleach.clean(request.form['post_body'])
-    parsed_post_body = safe_markdown.convert(request.form['post_body'])
-    blog_post = BlogPost(parsed_post_name, parsed_post_body)
-    #except:
-    #  abort(400)
-    db.session.add(blog_post)
-    db.session.commit()
-    redirect("/post/{}".format(blog_post.id), code=303)
+    try:
+      parsed_post_name = bleach.clean(request.form['post_title'])
+      parsed_post_body = safe_markdown.convert(request.form['post_body'])
+      print parsed_post_name, parsed_post_body
+      blog_post = BlogPost(parsed_post_name, parsed_post_body)
+      db.session.add(blog_post)
+      db.session.commit()
+    except:
+      abort(400)
+    return redirect("/post/{}".format(blog_post.post_id), code=303)
+    
 
-@application.route('/post/<int:post_id>')
-def show_post(post_id):
+@application.route('/post/<int:post_id>', methods=['GET', 'POST'])
+def blogpost(post_id):
   if request.method == 'GET':
-    blog_post = BlogPost.query.filter_by(id=str(id)).first()
-    return render_template('/campaign/show_campaign.html', blog_post=blog_post)
-  else:
-    abort(400)
+    try:
+      blog_post = BlogPost.query.get(post_id)
+      return render_template('post.html', blog_post=blog_post, comments=blog_post.comments)
+    except:
+      abort(404)
+  elif request.method == 'POST':
+    try:
+      parsed_comment_body = safe_markdown.convert(request.form['comment_body'])
+      comment = Comment(str(post_id), parsed_comment_body)
+      db.session.add(comment)
+      db.session.commit()
+      comment_id = comment.comment_id
+    except:
+      abort(400)
+    return redirect("/post/{}#{}".format(post_id,comment_id), code=303)
+  else: #this should never be called
+    abort(500)
 
+@application.route('/delete_post', methods=['POST'])
+def delete_post():
+  try:
+    blog_post = BlogPost.query.get(request.form['post_id'])
+    db.session.delete(blog_post)
+    db.session.commit()
+  except:
+    abort(404)
+  return redirect("/", code=303)
+
+@application.route('/delete_comment', methods=['POST'])
+def delete_comment():
+  try:
+    comment = Comment.query.get(request.form['comment_id'])
+    post_id = comment.post_id
+    db.session.delete(comment)
+    db.session.commit()
+  except:
+    abort(404)
+  return redirect("/post/{}".format(post_id), code=303)
 
 if __name__ == '__main__':
   application.run(debug=True)
--- models.py
+++ models.py
@@ -1,15 +1,23 @@
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask import Markup
 
 db = SQLAlchemy()
 
 class BlogPost(db.Model):
-  #__tablename__ = 'blogpost'
-  id            = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  timestamp     = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
+  post_id       = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  timestamp     = db.Column(db.DateTime, default=db.func.now())
   title         = db.Column(db.String)
   body          = db.Column(db.String)
 
-  #comments      = db.relationship('Comment', backref='blogpost', lazy='dynamic')
+  comments      = db.relationship('Comment', backref='blogpost', cascade="all,delete")
+
+  @property
+  def num_comments(self):
+    return db.session.query(Comment).with_parent(self, "comments").count()
+  @property
+  def pretty_time(self):
+      return self.timestamp.strftime("%B %d, %Y at %H:%M:%S")
+  
 
   def __init__(self, 
                title = None,
@@ -19,13 +27,18 @@ class BlogPost(db.Model):
     self.body  = body
 
 class Comment(db.Model):
-  #__tablename__ = 'comment'
-  id            = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  timestamp     = db.Column(db.DateTime, default=db.func.now(), onupdate=db.func.now())
-  #blogpost_id   = db.Column(db.Integer, db.ForeignKey('blogpost.id'))
+  comment_id    = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  timestamp     = db.Column(db.DateTime, default=db.func.now())
+  post_id       = db.Column(db.Integer, db.ForeignKey('blog_post.post_id'))
   body          = db.Column(db.String)
 
-  def __init__(self, 
+  @property
+  def pretty_time(self):
+      return self.timestamp.strftime("%B %d, %Y at %H:%M:%S")
+
+  def __init__(self,
+               post_id = None,
                body = None
               ):
-    self.body  = body
+    self.post_id = post_id
+    self.body        = body
