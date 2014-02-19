26cc68c244406265a477d0eedacc575d8c284781
Jacob H. Haven <QuicksilverJohny@gmail.com>
2014-02-19 11:27:09 -0800

    Documenting database models

--- models.py
+++ models.py
@@ -3,17 +3,21 @@ from flask import Markup
 
 db = SQLAlchemy()
 
+#Model for a blog post: 
 class BlogPost(db.Model):
   post_id       = db.Column(db.Integer, primary_key=True, autoincrement=True)
   timestamp     = db.Column(db.DateTime, default=db.func.now())
-  title         = db.Column(db.String)
-  body          = db.Column(db.String)
-
+  title         = db.Column(db.String) #HTML-escaped (using bleach) string
+  body          = db.Column(db.String) #Markdown formated string (also escapes html)
+  
+  #relationship to child comments. They will be cascade deleted upon post deletion
   comments      = db.relationship('Comment', backref='blogpost', cascade="all,delete")
 
+  #`blog_post.num_comments` counts the number of child comments this post has.
   @property
   def num_comments(self):
     return db.session.query(Comment).with_parent(self, "comments").count()
+  #`blog_post.pretty_time` is a pretty-printed timestamp: "MMM DD, YYYY at HH:MM:SS"
   @property
   def pretty_time(self):
       return self.timestamp.strftime("%B %d, %Y at %H:%M:%S")
@@ -26,12 +30,14 @@ class BlogPost(db.Model):
     self.title = title  
     self.body  = body
 
+#Model for a comment
 class Comment(db.Model):
   comment_id    = db.Column(db.Integer, primary_key=True, autoincrement=True)
   timestamp     = db.Column(db.DateTime, default=db.func.now())
   post_id       = db.Column(db.Integer, db.ForeignKey('blog_post.post_id'))
-  body          = db.Column(db.String)
+  body          = db.Column(db.String) #Markdown formated string (also escapes html)
 
+  #`comment.pretty_time` is a pretty-printed timestamp: "MMM DD, YYYY at HH:MM:SS"
   @property
   def pretty_time(self):
       return self.timestamp.strftime("%B %d, %Y at %H:%M:%S")
